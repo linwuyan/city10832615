6ba2ec1488eb48c10de17f99348156d3beb4e927
linwuyan <76465635+linwuyan@users.noreply.github.com>
2021-05-11 16:21:24 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://linwuyan.github.io/city10832615/model.json';
+let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
 
 
 function preload() {
